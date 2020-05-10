0255133b3d90f45bed5e8871f05d584d4985338b
Earu <earu.derg@gmail.com>
2020-05-10 04:01:59 +0200

    Fix leak

--- source/main.cpp
+++ source/main.cpp
@@ -24,7 +24,7 @@ const std::string get_toast_error(int enum_value)
 
 const std::wstring to_utf16(const char* utf8_str)
 {
-	std::string base_str(utf8_str);
+	const std::string base_str(utf8_str);
 	std::wstring utf16_str(base_str.size(), L' ');
 	utf16_str.resize(std::mbstowcs(&utf16_str[0], base_str.c_str(), base_str.size()));
 
@@ -41,7 +41,12 @@ public:
 
 LUA_FUNCTION(show_toast)
 {
-	bool has_image_path = LUA->Top() == 3;
+	if (!is_usable) {
+		LUA->PushBool(false);
+		return 1;
+	}
+
+	const bool has_image_path = LUA->Top() >= 3;
 	const char* title = LUA->CheckString(1);
 	const char* content = LUA->CheckString(2);
 
@@ -57,13 +62,12 @@ LUA_FUNCTION(show_toast)
 	{
 		const char* data_path = LUA->CheckString(3);
 		const std::wstring gmod_path = std::filesystem::current_path().wstring();
-
-		std::wstring full_path = gmod_path + L"/garrysmod/data/" + to_utf16(data_path);
+		const std::wstring full_path = gmod_path + L"/garrysmod/data/" + to_utf16(data_path);
 		templ.setImagePath(full_path);
 	}
 
-	bool failed = WinToastLib::WinToast::instance()->showToast(templ, new WinNotifsHandler()) < 0;
-	LUA->PushBool(!failed);
+	const bool success = WinToastLib::WinToast::instance()->showToast(templ, new WinNotifsHandler()) >= 0;
+	LUA->PushBool(success);
 	return 1;
 }
 
@@ -71,7 +75,7 @@ GMOD_MODULE_OPEN()
 {
 	if (!WinToastLib::WinToast::isCompatible())
 	{
-		LUA->ThrowError("[WinToasts] Your system is not supported!");
+		//LUA->ThrowError("[WinToasts] Your system is not supported!");
 		is_usable = false;
 		return 0;
 	}
@@ -82,8 +86,8 @@ GMOD_MODULE_OPEN()
 	WinToastLib::WinToast::WinToastError err;
 	if (!WinToastLib::WinToast::instance()->initialize(&err))
 	{
-		std::string err_msg = "[WinToasts] Could not initialize: " + get_toast_error(err);
-		LUA->ThrowError(err_msg.c_str());
+		//const std::string err_msg = "[WinToasts] Could not initialize: " + get_toast_error(err);
+		//LUA->ThrowError(err_msg.c_str());
 		is_usable = false;
 		return 0;
 	}
@@ -91,13 +95,13 @@ GMOD_MODULE_OPEN()
 	LUA->PushSpecial(GarrysMod::Lua::SPECIAL_GLOB);
 
 	LUA->CreateTable();
-	LUA->SetField(-2, "WinToast");
-
-	LUA->GetField(-1, "WinToast");
 	LUA->PushCFunction(show_toast);
 	LUA->SetField(-2, "Show");
+	LUA->SetField(-2, "WinToast");
 
 	LUA->Pop();
+
+	return 0;
 }
 
 GMOD_MODULE_CLOSE()
