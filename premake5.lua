d2eb422c1267fb9277340bc817923c5d5307f614
Earu <earu.derg@gmail.com>
2020-05-10 02:34:30 +0200

    actual first commit

---
+++ premake5.lua
@@ -0,0 +1,18 @@
+newoption({
+	trigger = "gmcommon",
+	description = "Sets the path to the garrysmod_common (https://github.com/danielga/garrysmod_common) directory",
+	value = "path to garrysmod_common directory"
+})
+
+local gmcommon = assert(_OPTIONS.gmcommon or os.getenv("GARRYSMOD_COMMON"),
+	"you didn't provide a path to your garrysmod_common (https://github.com/danielga/garrysmod_common) directory")
+include(path.join(gmcommon, "generator.v2.lua"))
+
+CreateWorkspace({name = "win_toast"})
+    CreateProject({serverside = false})
+        IncludeLuaShared()
+        includedirs({"include"})
+        files({
+            "include/*.cpp",
+            "include/*.h"
+        })
