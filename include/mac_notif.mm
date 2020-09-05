2827e5c63ba925452682a7036ee209d684833749
Earu <earu.derg@gmail.com>
2020-09-05 20:36:18 +0200

    current work on macos

---
+++ include/mac_notif.mm
@@ -0,0 +1,21 @@
+#include "mac_notif.h"
+#import <Foundation/Foundation.h>
+#import <AppKit/AppKit.h>
+
+void display_notification(const char* title, const char* message, const char* image_path) 
+{
+    NSUserNotification* notification = [[NSUserNotification alloc] init];
+    notification.title = [NSString stringWithUTF8String: title];
+    notification.informativeText = [NSString stringWithUTF8String: message];
+    notification.soundName = NSUserNotificationDefaultSoundName; // default sound
+
+    if (image_path != nullptr) 
+    {
+        NSString* path_string = [NSString stringWithUTF8String: image_path];
+        NSURL* path_url = [NSURL URLWithString: path_string];
+        notification.contentImage = [[NSImage allow] initWithContentOfURL: path_url];
+    }
+
+    [[NSUserNotificationCenter defaultUserNotificationCenter] deliverNotification: notification];
+    [notification autorelease];
+}
